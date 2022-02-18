22361a6b9bb78a17026ca8fa684ea9c31759fc27
Hookay <72449804+Hookay@users.noreply.github.com>
2022-02-18 01:07:27 +0000

    Update EX2-Web.js
    tidy notes

--- LAB2/EX2-Web/EX2-Web.js
+++ LAB2/EX2-Web/EX2-Web.js
@@ -1,11 +1,17 @@
- // makes sure that the round circle follows the mouse cursor
+//--------------------------follow--------------------------------
+
+// makes sure that the round circle follows the mouse cursor
  var f = document.getElementById('foo');
 			document.addEventListener('mousemove', function(ev){
  		  f.style.transform = 'translateY('+(ev.clientY)+'px)';
    		 f.style.transform += 'translateX('+(ev.clientX)+'px)';
 			},true);
       
-      
+  
+//---------------------------time---------------------------------  
+  
+  
+  
   //shows the actual time  https://codepen.io/afarrar/pen/JRaEjP
   function showTime(){
     var date = new Date();
